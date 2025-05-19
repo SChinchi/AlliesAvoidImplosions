e60699bff68bfcf58c63fb19e9f750f06f7562ba
SChinchi <57867641+SChinchi@users.noreply.github.com>
2025-05-19 15:07:04 +0100

    Code clean-up
    - Use `namespace Name;`.
    - Simplify implicit checks.
    - Use nameof for IL Hooks.
    - Apply all hooks on load. Toggling in OnEnable/OnDisable is pointless.
    - Add CHANGELOG to solution items.

--- AlliesAvoidImplosions/AlliesAvoidImplosions.cs
+++ AlliesAvoidImplosions/AlliesAvoidImplosions.cs
@@ -7,49 +7,37 @@ using System.Security.Permissions;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace AlliesAvoidImplosions
+namespace AlliesAvoidImplosions;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+[NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+public class AlliesAvoidImplosions : BaseUnityPlugin
 {
-    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
-    public class AlliesAvoidImplosions : BaseUnityPlugin
+    public const string PluginGUID = PluginAuthor + "." + PluginName;
+    public const string PluginAuthor = "Chinchi";
+    public const string PluginName = "AlliesAvoidImplosions";
+    public const string PluginVersion = "1.0.1";
+
+    internal static new BepInEx.Logging.ManualLogSource Logger;
+    internal static readonly string[] defaultAllies =
+    [
+        "Drone1Master",
+        "Drone2Master",
+        "DroneBackupMaster",
+        "DroneMissileMaster",
+        "EmergencyDroneMaster",
+        "EquipmentDroneMaster",
+        "FlameDroneMaster",
+        "MegaDroneMaster",
+        "Turret1Master",
+        "DevotedLemurianMaster"
+    ];
+
+    private void Awake()
     {
-        public const string PluginGUID = PluginAuthor + "." + PluginName;
-        public const string PluginAuthor = "Chinchi";
-        public const string PluginName = "AlliesAvoidImplosions";
-        public const string PluginVersion = "1.0.1";
-
-        internal static new BepInEx.Logging.ManualLogSource Logger;
-        internal static readonly string[] defaultAllies =
-        [
-            "Drone1Master",
-            "Drone2Master",
-            "DroneBackupMaster",
-            "DroneMissileMaster",
-            "EmergencyDroneMaster",
-            "EquipmentDroneMaster",
-            "FlameDroneMaster",
-            "MegaDroneMaster",
-            "Turret1Master",
-            "DevotedLemurianMaster"
-        ];
-
-        private void Awake()
-        {
-            Logger = base.Logger;
-            Configuration.Init(Config);
-            RoR2Application.onLoad += Hooks.LoadDataAndPatch;
-        }
-
-        private void OnEnable()
-        {
-            Hooks.Apply();
-            Hooks.CollectCurrentProjectiles();
-        }
-
-        private void OnDisable()
-        {
-            Hooks.Undo();
-            Hooks.implosions.Clear();
-        }
+        Logger = base.Logger;
+        Configuration.Init(Config);
+        Hooks.Init();
+        RoR2Application.onLoad += Hooks.LoadDataAndPatch;
     }
 }
--- AlliesAvoidImplosions/Hooks.cs
+++ AlliesAvoidImplosions/Hooks.cs
@@ -10,188 +10,163 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace AlliesAvoidImplosions
+namespace AlliesAvoidImplosions;
+
+internal class Hooks
 {
-    internal class Hooks
+    internal static readonly HashSet<GameObject> implosions = [];
+    private static readonly HashSet<int> implosionProjectiles = [];
+    private static readonly HashSet<MasterCatalog.MasterIndex> backupMasterIndices = [];
+    private static readonly HashSet<MasterCatalog.MasterIndex> immuneMasterIndices = [];
+
+    internal static void Init()
     {
-        internal static readonly HashSet<GameObject> implosions = [];
-        private static readonly HashSet<int> implosionProjectiles = [];
-        private static readonly HashSet<MasterCatalog.MasterIndex> backupMasterIndices = [];
-        private static readonly HashSet<MasterCatalog.MasterIndex> immuneMasterIndices = [];
-
-        internal static void Apply()
+        On.RoR2.Projectile.ProjectileController.Awake += OnSpawnProjectile;
+        On.RoR2.Projectile.ProjectileController.OnDestroy += OnDestroyProjectile;
+        if (Configuration.immuneToVoidDeath.Value)
         {
-            On.RoR2.Projectile.ProjectileController.Awake += OnSpawnProjectile;
-            On.RoR2.Projectile.ProjectileController.OnDestroy += OnDestroyProjectile;
-            if (Configuration.immuneToVoidDeath.Value)
+            IL.RoR2.HealthComponent.TakeDamageProcess += IgnoreVoidDeathForAllies;
+        }
+    }
+
+    private static void IgnoreVoidDeathForAllies(ILContext il)
+    {
+        var c = new ILCursor(il);
+        if (!c.TryGotoNext(
+            x => x.MatchLdfld<CharacterBody>(nameof(CharacterBody.bodyFlags)),
+            x => x.MatchLdcI4((int)CharacterBody.BodyFlags.ImmuneToVoidDeath)))
+        {
+            AlliesAvoidImplosions.Logger.LogError("Failed to patch " + il.Method.Name);
+            return;
+        }
+        c.Index += 1;
+        c.Emit(OpCodes.Ldarg_0);
+        c.EmitDelegate<Func<CharacterBody.BodyFlags, HealthComponent, CharacterBody.BodyFlags>>((bodyFlags, healthComponent) =>
+        {
+            var master = healthComponent.body.master;
+            if (master && immuneMasterIndices.Contains(master.masterIndex) && Configuration.immuneToVoidDeath.Value)
             {
-                IL.RoR2.HealthComponent.TakeDamageProcess += IgnoreVoidDeathForAllies;
+                bodyFlags |= CharacterBody.BodyFlags.ImmuneToVoidDeath;
+            }
+            return bodyFlags;
+        });
+    }
+
+    private static void OnSpawnProjectile(On.RoR2.Projectile.ProjectileController.orig_Awake orig, ProjectileController self)
+    {
+        orig(self);
+        if (NetworkServer.active && implosionProjectiles.Contains(self.catalogIndex))
+        {
+            implosions.Add(self.gameObject);
+            GTFOHController.EnableAll();
+        }
+    }
+
+    private static void OnDestroyProjectile(On.RoR2.Projectile.ProjectileController.orig_OnDestroy orig, ProjectileController self)
+    {
+        if (NetworkServer.active && implosions.Contains(self.gameObject))
+        {
+            implosions.Remove(self.gameObject);
+        }
+        orig(self);
+    }
+
+    internal static void LoadDataAndPatch()
+    {
+        BuildImplosionProjectileList();
+        BuildAllyIDs();
+        PatchAllies();
+    }
+
+    private static void BuildImplosionProjectileList()
+    {
+        var voidDeathType = (DamageTypeCombo)DamageType.VoidDeath;
+        foreach (var projectile in ProjectileCatalog.projectilePrefabProjectileControllerComponents)
+        {
+            if (projectile.TryGetComponent<ProjectileDamage>(out var damage) && (damage.damageType & voidDeathType) == voidDeathType)
+            {
+                implosionProjectiles.Add(projectile.catalogIndex);
+            }
+        }
+    }
+
+    private static void BuildAllyIDs()
+    {
+        BuildMasterIndices(Configuration.additionalBackupEntries, Configuration.blacklistedBackupEntries, backupMasterIndices);
+        BuildMasterIndices(Configuration.additionalImmuneEntries, Configuration.blacklistedImmuneEntries, immuneMasterIndices);
+    }
+
+    private static void BuildMasterIndices(ConfigEntry<string> additionalConfig, ConfigEntry<string> blacklistedConfig, HashSet<MasterCatalog.MasterIndex> indices)
+    {
+        var blacklisted = new HashSet<string>(SplitConfig(blacklistedConfig));
+        var allowed = new HashSet<string>();
+        foreach (var name in AlliesAvoidImplosions.defaultAllies)
+        {
+            if (!blacklisted.Contains(name))
+            {
+                allowed.Add(name);
+            }
+        }
+        foreach (var name in SplitConfig(additionalConfig))
+        {
+            if (!blacklisted.Contains(name))
+            {
+                allowed.Add(name);
+            }
+        }
+        foreach (var name in allowed)
+        {
+            var master = MasterCatalog.FindMasterPrefab(name);
+            if (master)
+            {
+                indices.Add(master.GetComponent<CharacterMaster>().masterIndex);
             }
         }
 
-        internal static void Undo()
+        static IEnumerable<string> SplitConfig(ConfigEntry<string> config)
         {
-            On.RoR2.Projectile.ProjectileController.Awake -= OnSpawnProjectile;
-            On.RoR2.Projectile.ProjectileController.OnDestroy -= OnDestroyProjectile;
-            if (Configuration.immuneToVoidDeath.Value)
-            {
-                IL.RoR2.HealthComponent.TakeDamageProcess -= IgnoreVoidDeathForAllies;
-            }
+            return config.Value.Split(',').Select(x => x.Trim());
         }
+    }
 
-        private static void IgnoreVoidDeathForAllies(ILContext il)
+    private static void PatchAllies()
+    {
+        foreach (var index in backupMasterIndices)
         {
-            var c = new ILCursor(il);
-            if (!c.TryGotoNext(
-                x => x.MatchLdfld<CharacterBody>("bodyFlags"),
-                x => x.MatchLdcI4((int)CharacterBody.BodyFlags.ImmuneToVoidDeath)))
+            var master = MasterCatalog.GetMasterPrefab(index);
+            if (master)
             {
-                AlliesAvoidImplosions.Logger.LogError("Failed to patch " + il.Method.Name);
-                return;
-            }
-            c.Index += 1;
-            c.Emit(OpCodes.Ldarg_0);
-            c.EmitDelegate<Func<CharacterBody.BodyFlags, HealthComponent, CharacterBody.BodyFlags>>((bodyFlags, healthComponent) =>
-            {
-                var master = healthComponent.body.master;
-                if (master && immuneMasterIndices.Contains(master.masterIndex) && Configuration.immuneToVoidDeath.Value)
+                var cm = master.GetComponent<CharacterMaster>();
+                // Stationary turrets can't escape so their AI shouldn't be modified
+                if (cm.bodyPrefab && (cm.bodyPrefab.TryGetComponent<CharacterMotor>(out _) || cm.bodyPrefab.TryGetComponent<RigidbodyMotor>(out _)))
                 {
-                    bodyFlags |= CharacterBody.BodyFlags.ImmuneToVoidDeath;
-                }
-                return bodyFlags;
-            });
-        }
+                    var originalSkillDrivers = master.GetComponents<AISkillDriver>();
 
-        private static void OnSpawnProjectile(On.RoR2.Projectile.ProjectileController.orig_Awake orig, ProjectileController self)
-        {
-            orig(self);
-            if (NetworkServer.active && implosionProjectiles.Contains(self.catalogIndex))
-            {
-                implosions.Add(self.gameObject);
-                GTFOHController.EnableAll();
-            }
-        }
+                    var component = master.AddComponent<AISkillDriver>();
+                    component.customName = "BackUpFromImplosion";
+                    component.skillSlot = SkillSlot.None;
+                    component.maxDistance = Configuration.evasionDistance.Value;
+                    component.moveTargetType = AISkillDriver.TargetType.Custom;
+                    component.aimType = AISkillDriver.AimType.AtMoveTarget;
+                    component.movementType = AISkillDriver.MovementType.FleeMoveTarget;
+                    component.shouldSprint = true;
+                    component.ignoreNodeGraph = true;
+                    component.driverUpdateTimerOverride = 1f;
 
-        private static void OnDestroyProjectile(On.RoR2.Projectile.ProjectileController.orig_OnDestroy orig, ProjectileController self)
-        {
-            if (NetworkServer.active && implosions.Contains(self.gameObject))
-            {
-                implosions.Remove(self.gameObject);
-            }
-            orig(self);
-        }
-
-        internal static void CollectCurrentProjectiles()
-        {
-            if (NetworkServer.active)
-            {
-                foreach (var projectile in InstanceTracker.GetInstancesList<ProjectileController>())
-                {
-                    if (implosionProjectiles.Contains(projectile.catalogIndex))
+                    // The skill drivers must be in a specific order and I do not know of a better way to
+                    // programmatically reorder them, so I remove and readd the ones I want at the end.
+                    foreach (var skillDriver in originalSkillDrivers)
                     {
-                        implosions.Add(projectile.gameObject);
-                    }
-                }
-            }
-        }
-
-        internal static void LoadDataAndPatch()
-        {
-            BuildImplosionProjectileList();
-            BuildAllyIDs();
-            PatchAllies();
-        }
-
-        private static void BuildImplosionProjectileList()
-        {
-            var voidDeathType = (DamageTypeCombo)DamageType.VoidDeath;
-            foreach (var projectile in ProjectileCatalog.projectilePrefabProjectileControllerComponents)
-            {
-                if (projectile.TryGetComponent<ProjectileDamage>(out var damage) && (damage.damageType & voidDeathType) == voidDeathType)
-                {
-                    implosionProjectiles.Add(projectile.catalogIndex);
-                }
-            }
-        }
-
-        private static void BuildAllyIDs()
-        {
-            BuildMasterIndices(Configuration.additionalBackupEntries, Configuration.blacklistedBackupEntries, backupMasterIndices);
-            BuildMasterIndices(Configuration.additionalImmuneEntries, Configuration.blacklistedImmuneEntries, immuneMasterIndices);
-
-        }
-
-        private static void BuildMasterIndices(ConfigEntry<string> additionalConfig, ConfigEntry<string> blacklistedConfig, HashSet<MasterCatalog.MasterIndex> indices)
-        {
-            var blacklisted = new HashSet<string>(SplitConfig(blacklistedConfig));
-            var allowed = new HashSet<string>();
-            foreach (var name in AlliesAvoidImplosions.defaultAllies)
-            {
-                if (!blacklisted.Contains(name))
-                {
-                    allowed.Add(name);
-                }
-            }
-            foreach (var name in SplitConfig(additionalConfig))
-            {
-                if (!blacklisted.Contains(name))
-                {
-                    allowed.Add(name);
-                }
-            }
-            foreach (var name in allowed)
-            {
-                var master = MasterCatalog.FindMasterPrefab(name);
-                if (master != null)
-                {
-                    indices.Add(master.GetComponent<CharacterMaster>().masterIndex);
-                }
-            }
-
-            static IEnumerable<string> SplitConfig(ConfigEntry<string> config)
-            {
-                return config.Value.Split(',').Select(x => x.Trim());
-            }
-        }
-
-        private static void PatchAllies()
-        {
-            foreach (var index in backupMasterIndices)
-            {
-                var master = MasterCatalog.GetMasterPrefab(index);
-                if (master != null)
-                {
-                    var cm = master.GetComponent<CharacterMaster>();
-                    // Stationary turrets can't escape so their AI shouldn't be modified
-                    if (cm.bodyPrefab && (cm.bodyPrefab.TryGetComponent<CharacterMotor>(out _) || cm.bodyPrefab.TryGetComponent<RigidbodyMotor>(out _)))
-                    {
-                        var originalSkillDrivers = master.GetComponents<AISkillDriver>();
-
-                        var component = master.AddComponent<AISkillDriver>();
-                        component.customName = "BackUpFromImplosion";
-                        component.skillSlot = SkillSlot.None;
-                        component.maxDistance = Configuration.evasionDistance.Value;
-                        component.moveTargetType = AISkillDriver.TargetType.Custom;
-                        component.aimType = AISkillDriver.AimType.AtMoveTarget;
-                        component.movementType = AISkillDriver.MovementType.FleeMoveTarget;
-                        component.shouldSprint = true;
-                        component.ignoreNodeGraph = true;
-                        component.driverUpdateTimerOverride = 1f;
-
-                        // The skill drivers must be in a specific order and I do not know of a better way to
-                        // programmatically reorder them, so I remove and readd the ones I want at the end.
-                        foreach (var skillDriver in originalSkillDrivers)
+                        // The AISkillDriver can be subclassed so we can't rely on generics
+                        var copy = master.AddComponent(skillDriver.GetType());
+                        foreach (var field in skillDriver.GetType().GetFields())
                         {
-                            var copy = master.AddComponent<AISkillDriver>();
-                            foreach (var field in skillDriver.GetType().GetFields())
-                            {
-                                field.SetValue(copy, field.GetValue(skillDriver));
-                            }
-                            Component.DestroyImmediate(skillDriver);
+                            field.SetValue(copy, field.GetValue(skillDriver));
                         }
-
-                        master.AddComponent<GTFOHController>();
+                        UnityEngine.Object.DestroyImmediate(skillDriver);
                     }
+
+                    master.AddComponent<GTFOHController>();
                 }
             }
         }
